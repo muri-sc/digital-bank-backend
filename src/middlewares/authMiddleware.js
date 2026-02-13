3651bf78831d7e80c98620bc086e098f9ef0010c
muri-sc <18ferreiramurillo@gmail.com>
2026-02-13 00:19:17 -0300

    Correção no retorno de erros

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,13 +4,13 @@ export const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization
 
     if (!authHeader) {
-        return res.status(401).json({ message: "Token not provided" })
+        return res.status(401).json({ error: "Token not provided" })
     }
 
     const token = authHeader.split(' ')[1]
 
     if (!token) {
-        return res.status(401).json({ message: "Invalid token" })
+        return res.status(401).json({ error: "Invalid token" })
     }
 
     try {
@@ -20,6 +20,6 @@ export const authMiddleware = (req, res, next) => {
 
         return next()
     } catch (error) {
-        return res.status(401).json({ message: "Invalid or expired token" })
+        return res.status(401).json({ error: "Invalid or expired token" })
     }
 }
